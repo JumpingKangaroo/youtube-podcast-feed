586b4fca59344399478e9dff5016c4e0c95e04f4
Chris Lambertson <lambertsonchris@gmail.com>
2019-05-16 18:50:13 -0400

    Finish download job modification to fix file extension error

--- app/jobs/download_youtube_audio_job.rb
+++ app/jobs/download_youtube_audio_job.rb
@@ -1,13 +1,19 @@
 class DownloadYoutubeAudioJob < ApplicationJob
   queue_as :default
-
+  
   def perform(id, video)
-    result = `youtube-dl -x --audio-format "mp3" -o /home/tempDownloads/#{id}.mkv --restrict-filenames https://www.youtube.com/watch?v=#{id}`
+    # Use youtube-dl to download the video and convert to mp3 format
+    result = `youtube-dl -x --audio-format "mp3" -o '/home/tempDownloads/%(title)s-----%(id)s.%(ext)s' https://www.youtube.com/watch?v=#{id}`
+    # Extract the video title from the console output
+    expression = /\/home\/tempDownloads\/(.*?)-----/im
+    title = result.match expression
+    # Attach the audio file to the given video with ActiveStorage
     video.youtube_audio.attach(
       io: File.open("/home/tempDownloads/#{id}.mp3"),
       filename: "#{id}.mp3",
       content_type: 'audio/mpeg'
     )
-    File.delete("/home/tempDownloads/#{id}.mp3") if File.exist?("/home/tempDownloads/#{id}.mp3")
+    # Clean up the mp3 file from the download directory
+    File.delete(".mp3")
   end
 end
